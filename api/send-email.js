a7ea61afb68bbf3f7422cce7c1dbb6617991ff8e
Rajapandi03 <pandiraja981@gmail.com>
2026-01-18 19:22:25 +0530

    updated your backend code as requested.

--- api/send-email.js
+++ api/send-email.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
     await resend.emails.send({
       from: "VelWorld Enquiry <no-reply@velworld.net>",
-      to: ["team@velworld.net"],
+      to: ["velworld7@gmail.com"],
       reply_to: data.email,
       subject: "New Website Enquiry",
       html: `
